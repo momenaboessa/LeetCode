85a2c89a8f8379fc38438be9c51cdd5e435cc5b4
Momen Aboessa <77535066+momenaboessa@users.noreply.github.com>
2024-05-18 14:55:25 +0300

    Update 27. Remove Element.cs

--- 27. Remove Element.cs
+++ 27. Remove Element.cs
@@ -1,15 +1,18 @@
-public class Solution {
-    public int RemoveElement(int[] nums, int val)
+public class Solution 
+{
+    public int RemoveElement(int[] nums, int val) 
     {
-        if (nums.Length == 0)
-				return 0;
+        int k = 0; // Pointer for the next valid element position
 
-        int startingPoint = 0;
+        for (int i = 0; i < nums.Length; i++) 
+        {
+            if (nums[i] != val) 
+            {
+                nums[k] = nums[i]; // Move valid element to the front
+                k++; // Increment the count of valid elements
+            }
+        }
 
-        for (int secondPoint = 0; secondPoint < nums.Length; secondPoint++)
-            if (nums[secondPoint] != val)
-                nums[startingPoint++] = nums[secondPoint];
-
-        return startingPoint;
+        return k; // Return the number of valid elements
     }
 }
