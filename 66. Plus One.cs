3804a1beac5f923280927ff4f098893435ac5166
Momen Aboessa <77535066+momenaboessa@users.noreply.github.com>
2024-05-18 14:28:39 +0300

    Update 66. Plus One.cs

--- 66. Plus One.cs
+++ 66. Plus One.cs
@@ -1,19 +1,15 @@
 public class Solution {
-    public int[] PlusOne(int[] digits)
-    {
-        int ArrayLength = digits.Length;
-        for (int i = ArrayLength - 1; i >= 0; i--)
-        {
-            if (digits[i] < 9)
-            {
+    public int[] PlusOne(int[] digits) {
+        for (int i = digits.Length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
                 digits[i]++;
                 return digits;
             }
             digits[i] = 0;
         }
-        int[] newNumber = new int[ArrayLength + 1];
+        // All digits were 9
+        int[] newNumber = new int[digits.Length + 1];
         newNumber[0] = 1;
-
         return newNumber;
     }
 }
